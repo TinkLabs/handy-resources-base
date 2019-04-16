e9963eeb25e27f0226c4d6a98ee70c4b7abdee53
YouAnCao <lyon.cao@hi.com>
2019-04-16 18:09:28 +0800

    GUTS-6773#新增otapackage 与 hotelapps的数据库映射model

---
+++ src/main/java/com/tinklabs/handy/soa/resources/base/bean/HotelApp.java
@@ -0,0 +1,152 @@
+package com.tinklabs.handy.soa.resources.base.bean;
+
+/**
+ * @description: 酒店app信息
+ * @author: lyon.cao
+ * @date: 2019年4月16日
+ * @version: v.0.0.1
+ * @copyright: hi inc
+ */
+public class HotelApp {
+
+    private Integer id;
+
+    private String  image_url;
+
+    private String  package_url;
+
+    private String  package_name;
+
+    private String  md5sum;
+
+    private String  title;
+
+    private String  description;
+
+    private Integer order;
+
+    private Integer country_id;
+
+    private Integer hotel_id;
+
+    private String  locale;
+
+    private String  created;
+
+    private String  modified;
+
+    private String  sha256sum;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getPackage_url() {
+        return package_url;
+    }
+
+    public void setPackage_url(String package_url) {
+        this.package_url = package_url;
+    }
+
+    public String getPackage_name() {
+        return package_name;
+    }
+
+    public void setPackage_name(String package_name) {
+        this.package_name = package_name;
+    }
+
+    public String getMd5sum() {
+        return md5sum;
+    }
+
+    public void setMd5sum(String md5sum) {
+        this.md5sum = md5sum;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    public Integer getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(Integer country_id) {
+        this.country_id = country_id;
+    }
+
+    public Integer getHotel_id() {
+        return hotel_id;
+    }
+
+    public void setHotel_id(Integer hotel_id) {
+        this.hotel_id = hotel_id;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+
+    public String getSha256sum() {
+        return sha256sum;
+    }
+
+    public void setSha256sum(String sha256sum) {
+        this.sha256sum = sha256sum;
+    }
+
+}
---
+++ src/main/java/com/tinklabs/handy/soa/resources/base/bean/OtaPackage.java
@@ -0,0 +1,82 @@
+package com.tinklabs.handy.soa.resources.base.bean;
+
+/**
+ * @description: ota package 表的映射
+ * @author: lyon.cao
+ * @date: 2019年4月16日
+ * @version: v.0.0.1
+ * @copyright: hi inc
+ */
+public class OtaPackage {
+
+    private Integer id;
+
+    private String  name;
+
+    private String  package_name;
+
+    private Integer hotel_selectable;
+
+    private String  created;
+
+    private String  modified;
+
+    private Integer show_in_app_store;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPackage_name() {
+        return package_name;
+    }
+
+    public void setPackage_name(String package_name) {
+        this.package_name = package_name;
+    }
+
+    public Integer getHotel_selectable() {
+        return hotel_selectable;
+    }
+
+    public void setHotel_selectable(Integer hotel_selectable) {
+        this.hotel_selectable = hotel_selectable;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getModified() {
+        return modified;
+    }
+
+    public void setModified(String modified) {
+        this.modified = modified;
+    }
+
+    public Integer getShow_in_app_store() {
+        return show_in_app_store;
+    }
+
+    public void setShow_in_app_store(Integer show_in_app_store) {
+        this.show_in_app_store = show_in_app_store;
+    }
+
+}
